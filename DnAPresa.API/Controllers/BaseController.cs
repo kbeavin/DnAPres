a7ebda02b18b0ca1e3ff17c48b948bfea1ef268a
kbeavin <kbeavin@carter-logistics.com>
2020-01-08 17:23:11 -0500

    getting rid of ui related files & nuget packages & enabling cors

---
+++ DnAPresa.API/Controllers/BaseController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Carter.HondaCrossdock.UI.Controllers
+{
+    public class BaseController : ApiController
+    {
+        public static HttpClient HttpClient = new HttpClient(new HttpClientHandler { UseDefaultCredentials = true });
+        public readonly ClaimsPrincipal _User;
+        public readonly HttpContext _HttpContext;
+
+
+        public BaseController()
+        {
+            IHttpContextAccessor _httpContextAccessor = new HttpContextAccessor();
+            _HttpContext = _httpContextAccessor.HttpContext;
+            _User = _httpContextAccessor.HttpContext.User;
+
+            if (string.IsNullOrEmpty(HttpClient.BaseAddress?.ToString()))
+            {
+                HttpClient.BaseAddress = new Uri("http://localhost:57547/");
+            }
+
+            HttpClient.DefaultRequestHeaders.Accept.Clear();
+            HttpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+        }
+
+        public async Task<HttpResponseMessage> SendAPIRequest(string endPoint, string payload, HttpMethod httpMethod = null)
+        {
+            try
+            {
+                HttpResponseMessage response = null;
+
+                if (httpMethod == null)
+                {
+                    httpMethod = HttpMethod.Post;
+                }
+
+                HttpRequestMessage request = new HttpRequestMessage(httpMethod, endPoint)
+                {
+                    Content = new StringContent(payload,
+                        Encoding.UTF8,
+                        "application/json")
+                };
+
+                await HttpClient.SendAsync(request).ContinueWith(responseTask =>
+                {
+                    response = responseTask.Result;
+                });
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+
+                return null;
+            }
+        }
+    }
+}
---
+++ DnAPresa.API/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using DnAPresa.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace DnAPresa.API.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public async Task<DTO<Employee>> Get_FilteredEmployees()
+        {
+            DTO<Employee> dto = new DTO<Employee>();
+
+            try
+            {
+                HttpResponseMessage httpResponseMessage = await HttpClient.GetAsync("/api/Get_FilteredEmployees");
+            }
+
+            return dto;
+        }
+    }
+}
